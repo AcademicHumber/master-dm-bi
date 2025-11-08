1f8e64d3446de0a779bed7b670632cc4ede22750
Adrian Fernandez <ahft_2000@hotmail.com>
2025-11-07 22:02:31 -0400

    Agregando archivos

---
+++ Pubs.OLTP/Tables/authors.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[authors]
+(
+	[au_id] INT NOT NULL PRIMARY KEY
+	, [au_lname] NVARCHAR(40) NOT NULL
+	, [au_fname] NVARCHAR(20) NOT NULL
+	, [phone] CHAR(12) NOT NULL
+	, [address] NVARCHAR(40)
+	, [city] NVARCHAR(20)
+	, [state] NVARCHAR(2)
+	, [zip] NVARCHAR(10)
+	, [contract] BIT NOT NULL
+)
