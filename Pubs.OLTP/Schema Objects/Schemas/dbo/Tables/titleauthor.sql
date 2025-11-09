d04a2d5e6a4763446b81820fc7e319a6dd7b0f06
Jose Carlos Camacho Salazar <50775349+J-Carlos-CS@users.noreply.github.com>
2025-11-09 11:22:14 -0400

    Add titles and titleauthor tables to schema
    Created new tables [dbo].[titles] and [dbo].[titleauthor] with appropriate columns and primary keys. Updated the project file to include these tables and recorded column renames in the refactor log for better clarity and consistency.

---
+++ Pubs.OLTP/Schema Objects/Schemas/dbo/Tables/titleauthor.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[titleauthor]
+(
+	[au_id] INT NOT NULL PRIMARY KEY, 
+    [title_id] NCHAR(10) NOT NULL PRIMARY KEY, 
+    [au_ord] TINYINT NULL, 
+    [royaltyper] INT NULL, 
+    [RowVersion] TIMESTAMP NOT NULL
+)
---
+++ Pubs.OLTP/Schema Objects/Schemas/dbo/Tables/titles.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[titles]
+(
+	[title_id] INT NOT NULL PRIMARY KEY, 
+    [title] VARCHAR(80) NULL, 
+    [type] CHAR(12) NULL, 
+    [pub_id] CHAR(4) NULL, 
+    [price] MONEY NULL, 
+    [advance] MONEY NULL, 
+    [royalty] INT NULL, 
+    [ytd_sales] INT NULL, 
+    [notes] VARCHAR(200) NULL, 
+    [pubdate] DATETIME NULL, 
+    [RowVersion] TIMESTAMP NULL
+)
