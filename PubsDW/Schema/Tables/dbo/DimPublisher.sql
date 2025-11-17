7adde327d3b66f3dd9039058866786527f6c39e9
Perseo <yoh.chrisitan@gmail.com>
2025-11-16 21:46:15 -0400

    feat: create model star using scripts SQL in new project for DW

---
+++ PubsDW/Schema/Tables/dbo/DimPublisher.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DimPublisher]
+(
+    [PublisherKey] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [PublisherID_NK] CHAR(4) NOT NULL,
+    [PublisherName] VARCHAR(40) NULL,
+    [City] VARCHAR(20) NULL,
+    [State] CHAR(2) NULL,
+    [Country] VARCHAR(30) NULL,
+    [PublisherInfo] TEXT NULL
+);
+GO
