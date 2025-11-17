f804552d1c1f1df1d894b64bd3a324584b4c174b
Perseo <yoh.chrisitan@gmail.com>
2025-11-16 22:35:35 -0400

    feat: update model star

--- PubsDW/Schema/Tables/dbo/FactSales.sql
+++ PubsDW/Schema/Tables/dbo/FactSales.sql
@@ -12,18 +12,10 @@
 
     [QuantitySold] SMALLINT NOT NULL, 
     [UnitPrice] MONEY NOT NULL, 
-    [TotalSaleAmount] MONEY NOT NULL
+    [TotalSaleAmount] MONEY NOT NULL,
+    CONSTRAINT [FK_FactSales_DimDate] FOREIGN KEY ([DateKey]) REFERENCES [dbo].[DimDate]([DateKey]),
+    CONSTRAINT [FK_FactSales_DimStore] FOREIGN KEY ([StoreKey]) REFERENCES [dbo].[DimStore]([StoreKey]),
+    CONSTRAINT [FK_FactSales_DimTitle] FOREIGN KEY ([TitleKey]) REFERENCES [dbo].[DimTitle]([TitleKey]),
+    CONSTRAINT [FK_FactSales_DimPublisher] FOREIGN KEY ([PublisherKey]) REFERENCES [dbo].[DimPublisher]([PublisherKey]),
 );
 GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT [FK_FactSales_DimDate]
-    FOREIGN KEY ([DateKey]) REFERENCES [dbo].[DimDate]([DateKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT [FK_FactSales_DimStore]
-    FOREIGN KEY ([StoreKey]) REFERENCES [dbo].[DimStore]([StoreKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT [FK_FactSales_DimTitle]
-    FOREIGN KEY ([TitleKey]) REFERENCES [dbo].[DimTitle]([TitleKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT [FK_FactSales_DimPublisher]
-    FOREIGN KEY ([PublisherKey]) REFERENCES [dbo].[DimPublisher]([PublisherKey]);
-    GO
