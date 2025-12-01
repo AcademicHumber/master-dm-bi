4f236d27bc310dc71416bf2271c7735a71e2bd88
Adrian Fernandez <ahft_2000@hotmail.com>
2025-12-01 18:14:32 -0400

    Start ETL

---
+++ Pubs.OLTP/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAuthorsChangeByRowVersion.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[GetAuthorsChangeByRowVersion]
+(
+	@startRow BIGINT,
+	@endRow   BIGINT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT a.[au_id]
+          ,a.[au_lname]
+          ,a.[au_fname]
+          ,a.[phone]
+          ,a.[address]
+          ,a.[city]
+          ,a.[state]
+          ,a.[zip]
+          ,a.[contract]
+		  ,a.[RowVersion]
+    FROM [dbo].[authors] AS a
+    WHERE a.[RowVersion] >  CONVERT(rowversion, @startRow)
+      AND a.[RowVersion] <= CONVERT(rowversion, @endRow);
+END
+GO
